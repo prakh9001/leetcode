fa837225b727b4da0a6ec74586703e8f5cdc1520
Aniket Kumar Jha <117656460+prakh9001@users.noreply.github.com>
2023-10-16 23:24:46 +0530

    Time: 0 ms (100.00%), Space: 43.6 MB (91.50%) - LeetHub

---
+++ 0268-missing-number/0268-missing-number.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int missingNumber(int[] nums) {
+        int n = nums.length;
+        int arr[] = new int[n+1];
+        for(int i = 0 ; i<n; i++){
+            arr[nums[i]]=1; 
+        }
+        for(int i = 0; i<=n; i++){
+            if(arr[i]==0){
+                return i;
+            }
+        }
+        return -1;
+    }
+}
